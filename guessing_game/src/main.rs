ecb03b00d1f02a51b78ef07e47619d1b578174a4
Nikola Totev <nikola.r.totev@gmail.com>
2023-06-28 21:46:47 +0300

    feat: Create guessing game

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,4 +1,38 @@
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guessing game");
+
+    let secret_number: u32 = rand::thread_rng().gen_range(1..=100);
+    println!("The secret number is: {secret_number}");
+
+    loop {
+        println!("Enter a number");
+
+        let mut guess: String = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+        //&mut guess means guess is passed as a raference
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Uh-oh, this isn't a number!");
+                continue;
+            }
+        };
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Ooof a bit too low"),
+            Ordering::Greater => println!("Ah, a bit too high"),
+            Ordering::Equal => {
+                println!("Perfecto!");
+                break;
+            }
+        }
+    }
 }
- 
